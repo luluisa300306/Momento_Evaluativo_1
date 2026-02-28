9bc8374f8ef2d8164cf3d2d2fbb45d3b41f9f4e0
luisa <luisatequierema@gmail.com>
2026-02-28 00:15:18 -0500

    Calcular proyecto funcional

--- funciones.js
+++ funciones.js
@@ -1,5 +1,36 @@
-document.getElementById("CalculadoradeViabilidaddeProyecto").addEventListener("click, CalcularProyecto");
+document.getElementById("viabilidadProyecto").addEventListener("click", calcularProyecto);
 
-function CalculadoradeViabilidaddeProyecto() {
-    
+function calcularProyecto() {
+
+    let tipoServicio = document.getElementById("servicio").value;
+    let numeroSemanas = parseInt(document.getElementById("numeroSemanas").value);
+
+
+    let estado = ""
+    let mensaje = ""
+
+    if (!tipoServicio || !numeroSemanas) {
+        alert("Por favor complete todos los campos");
+        return;
+    }
+
+    if (numeroSemanas < 5) {
+        estado = "NO VIABLE"
+        mensaje = "El tiempo es demasiado corto para un proyecto de este tipo.";
+    }
+    else if (numeroSemanas < 7) {
+        estado = "RIESGOSO";
+        mensaje = "Se puede intentar, pero hay alto riesgo de fallas o retrasos.";
+    }
+    else if (numeroSemanas < 9) {
+        estado = "VIABLE";
+        mensaje = "El tiempo estimado es adecuado para desarrollar el proyecto.";
+    }
+    else {
+        estado = "MUY VIABLE";
+        mensaje = "Hay tiempo suficiente para un desarrollo sólido y de calidad.";
+    }
+
+    document.getElementById("estadoProyecto").textContent = estado;
+    document.getElementById("mensajeProyecto").textContent = mensaje;
 }
