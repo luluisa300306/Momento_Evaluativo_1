0591dac4aa3375ae474c809aa4cae798b3fa409a
luisa <luisatequierema@gmail.com>
2026-02-27 23:20:04 -0500

    arreglos tabla en html y css 1

--- funciones.js
+++ funciones.js
@@ -1,28 +1,5 @@
-function calculartiempodeproyecto() {
-    var secciones = prompt("Ingrese el número de servicio a tomar del sitio web");
-    var complejidad = prompt("Ingrese el nivel de complejidad de la pieza siendo 1 la mas baja y 3 la mas alta: 1 (baja), 2 (media), 3 (alta)");
+document.getElementById("CalculadoradeViabilidaddeProyecto").addEventListener("click, CalcularProyecto");
 
-    if (secciones == "" || complejidad == "") {
-        alert("Debe ingresar todos los datos");
-        return;
-    }
-
-secciones = parseInt(secciones);
-complejidad = parseInt(complejidad);
-
-let tiempo = secciones * complejidad;
-let mensaje = "";
-
-if (tiempo < 5) {
-    mensaje = "Proyecto corto (entrega rápida)";
-    } else if (tiempo < 12) {
-        mensaje= "Proyecto de duración media";
-    } else {
-        mensaje = "Proyecto largo (alta dedicación)";
-    }
-
-    alert("Tiempo estimado del proyecto: " + mensaje);
-    console.log("Secciones:", secciones);
-    console.log("Complejidad:", complejidad);
-    console.log("Resultado:", mensaje);
+function CalculadoradeViabilidaddeProyecto() {
+    
 }
